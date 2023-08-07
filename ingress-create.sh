d70cba554ce6f560cf3979f748681ade87e16f0c
Stephen Burke <se7enack@users.noreply.github.com>
2023-08-07 15:56:14 -0400

    Update ingress-create.sh

--- ingress-create.sh
+++ ingress-create.sh
@@ -1,6 +1,6 @@
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
-helm upgrade dca0kafka-ingress ingress-nginx/ingress-nginx \
+helm upgrade kafka-ingress ingress-nginx/ingress-nginx \
 --version 4.7.0 \
 --atomic \
 --namespace kafka \
@@ -21,5 +21,5 @@ helm upgrade dca0kafka-ingress ingress-nginx/ingress-nginx \
 --set controller.resources.requests.cpu=.01 \
 --set controller.config.hide-headers=Server
 sleep 5 
-kubectl --namespace kafka get services -o json dca0kafka-ingress-ingress-nginx-controller | jq -r '.status.loadBalancer.ingress[0].ip'
+kubectl --namespace kafka get services -o json kafka-ingress-ingress-nginx-controller | jq -r '.status.loadBalancer.ingress[0].ip'
  
